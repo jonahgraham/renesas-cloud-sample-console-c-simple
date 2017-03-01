5cb2ea2cb311515156e65dd72ae317520ac65481
Jonah Graham <jonah@kichwacoders.com>
2017-03-01 12:15:30 +0000

    Sample Hello World in C for e2cloud

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	$(CC) main.c -o console-c-simple
+
+clean:
+	$(RM) console-c-simple
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	printf("Hello from e2cloud\n");
+	return 0;
+}
